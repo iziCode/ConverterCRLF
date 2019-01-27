4ea3c562b9948e628ec9fb214f3c96e4457644d2
Egor <Unikovskiy@yandex.ru>
2019-01-28 00:12:19 +0300

    func showAllFormats works fine

--- showAllFilesAndFormats.go
+++ showAllFilesAndFormats.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	log "github.com/sirupsen/logrus"
+	"io/ioutil"
 )
 
 func ShowAllFilesAndFormats() {
@@ -11,8 +12,9 @@ func ShowAllFilesAndFormats() {
 
 	allFilesPaths := GetAllFilesFromPath(filePath)
 
-	getFileFormatNEL(allFilesPaths)
+	allFilesAndFormats := getFileFormatNEL(allFilesPaths)
 
+	fmt.Println(allFilesAndFormats)
 }
 
 func ReadShowAllFilesAndFormats() (filePath string) {
@@ -31,6 +33,26 @@ func ReadShowAllFilesAndFormats() (filePath string) {
 }
 
 func getFileFormatNEL(allFilesPaths []string) (allFilesAndFormats map[string]string) {
+	funcName := "getFileFormatNEL(allFilesPaths []string) (allFilesAndFormats map[string]string)"
+	allFilesAndFormats = make(map[string]string)
+	for _, filePath := range allFilesPaths {
+		dataBytes, err := ioutil.ReadFile(filePath)
+		CheckErrors(funcName, err)
 
-	return
+		for i := 0; i < len(dataBytes); i++ {
+
+			if dataBytes[i] == 13 && i+1 < len(dataBytes) && dataBytes[i+1] == 10 {
+				allFilesAndFormats[filePath] = CRLF
+				break
+			} else if dataBytes[i] == 13 {
+				allFilesAndFormats[filePath] = CR
+				break
+			} else if dataBytes[i] == 10 {
+				allFilesAndFormats[filePath] = LF
+				break
+			}
+		}
+
+	}
+	return allFilesAndFormats
 }
